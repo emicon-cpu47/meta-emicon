24cd0980df70e85212f9d2502a9e27c684ca35f3
Mikhail Shvetsov <lameli67@gmail.com>
2019-02-20 12:05:38 +0300

    update_codesys_reboot.sh: cds_libs added

--- meta-variscite-amx3-em/recipes-lcd-menu-cpu47/lcd-menu-cpu47/lcd-menu-cpu47/update_codesys_reboot.sh
+++ meta-variscite-amx3-em/recipes-lcd-menu-cpu47/lcd-menu-cpu47/lcd-menu-cpu47/update_codesys_reboot.sh
@@ -10,6 +10,14 @@ if [ -d "PlcLogic" ]; then
 	rm -r /home/plc/PlcLogic
 	mv PlcLogic /home/plc/PlcLogic
 fi
+
+if [ -d "cds_lib" ]; then
+	chgrp root cds_lib -R
+	chmod g+w cds_lib -R
+	cp cds_lib/* /usr/lib/
+	rm -r cds_lib
+fi
+
 cd CODESYSControl
 chgrp root *
 chmod g+w *
