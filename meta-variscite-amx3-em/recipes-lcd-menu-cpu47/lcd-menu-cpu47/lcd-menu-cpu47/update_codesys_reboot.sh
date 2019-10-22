40d1e6b46e50054cbee00b1047997636f14249f7
Mike Shvetsov <m.shvetsov@prolog-plc.ru>
2019-10-22 22:47:27 +0200

    update_codesys_reboot.sh: path fixed

--- meta-variscite-amx3-em/recipes-lcd-menu-cpu47/lcd-menu-cpu47/lcd-menu-cpu47/update_codesys_reboot.sh
+++ meta-variscite-amx3-em/recipes-lcd-menu-cpu47/lcd-menu-cpu47/lcd-menu-cpu47/update_codesys_reboot.sh
@@ -6,11 +6,12 @@
 cd /usr/bin/codesys
 tar -xf update.tar
 rm update.tar
+
 if [ -d "PlcLogic" ]; then
 	chgrp root PlcLogic -R
 	chmod g+w PlcLogic -R
 	rm -r /home/plc/PlcLogic
-	mv PlcLogic /home/plc/PlcLogic
+	mv PlcLogic /home/plc/
 fi
 
 if [ -d "cds_lib" ]; then
